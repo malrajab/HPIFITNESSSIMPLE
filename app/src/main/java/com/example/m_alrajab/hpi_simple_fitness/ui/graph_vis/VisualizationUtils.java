0d81b5d62eb5dca580021fedea819f95d2f63205
m_alrajab <moaath78@gmail.com>
2017-01-18 00:21:32 -0500

    Add a method to parse a uri to resources
    This method has been added to the visualizationUtils class to parse drawable pictures inside the graph

--- app/src/main/java/com/example/m_alrajab/hpi_simple_fitness/ui/graph_vis/VisualizationUtils.java
+++ app/src/main/java/com/example/m_alrajab/hpi_simple_fitness/ui/graph_vis/VisualizationUtils.java
@@ -1,5 +1,12 @@
 package com.example.m_alrajab.hpi_simple_fitness.ui.graph_vis;
 
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.res.Resources;
+import android.net.Uri;
+import android.support.annotation.AnyRes;
+import android.support.annotation.NonNull;
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -16,5 +23,16 @@ public class VisualizationUtils {
 
     public static String getDateFormated(long end, int shift) {
         return new SimpleDateFormat(DATE_FORMAT_HPI).format(end - (shift - 1) * (24L * 3_600_000L));
+    }
+
+    public static final Uri getUriToResource(@NonNull Context context, @AnyRes int resId)
+            throws Resources.NotFoundException {
+        Resources res = context.getResources();
+        Uri resUri = Uri.parse(ContentResolver.SCHEME_ANDROID_RESOURCE +
+                "://" + res.getResourcePackageName(resId)
+                + '/' + res.getResourceTypeName(resId)
+                + '/' + res.getResourceEntryName(resId));
+
+        return resUri;
     }
 }
