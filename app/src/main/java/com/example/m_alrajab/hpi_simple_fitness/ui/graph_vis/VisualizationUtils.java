3438162da82c8a97889bb71b0d1b28f56852de19
m_alrajab <moaath78@gmail.com>
2017-01-18 08:23:34 -0500

    Fix minor typo
    Fix of a minor bug in the code

--- app/src/main/java/com/example/m_alrajab/hpi_simple_fitness/ui/graph_vis/VisualizationUtils.java
+++ app/src/main/java/com/example/m_alrajab/hpi_simple_fitness/ui/graph_vis/VisualizationUtils.java
@@ -11,6 +11,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 /**
+ * Utility class for praparing the visulization
  * Created by m_alrajab on 1/17/17.
  */
 
@@ -22,7 +23,8 @@ public class VisualizationUtils {
     }
 
     public static String getDateFormated(long end, int shift) {
-        return new SimpleDateFormat(DATE_FORMAT_HPI).format(end - (shift - 1) * (24L * 3_600_000L));
+        return new SimpleDateFormat(DATE_FORMAT_HPI).format(end
+                - (shift - 1) * (24L * 3_600_000L));
     }
 
     public static final Uri getUriToResource(@NonNull Context context, @AnyRes int resId)
